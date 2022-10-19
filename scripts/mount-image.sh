ad1996203ada47fe8ebbdac5ae8c103797e3afd6
Jakob Koschel <jkl820.git@gmail.com>
2022-10-19 17:18:44 +0200

    mount-image: also mount /boot/efi

--- scripts/mount-image.sh
+++ scripts/mount-image.sh
@@ -8,14 +8,17 @@ if [[ -z "$IMAGE" || -z "$MNT" ]]; then
 fi
 
 LOOP_DEV=$(sudo losetup --show -f $IMAGE)
-LOOP_PARTITION=${LOOP_DEV}p1
+LOOP_PARTITION_EFI=${LOOP_DEV}p1
+LOOP_PARTITION_LINUX=${LOOP_DEV}p2
 
 echo "LOOP_DEV: $LOOP_DEV"
 
 sudo partprobe $LOOP_DEV
 
 mkdir -p $MNT
-sudo mount $LOOP_PARTITION $MNT
+sudo mount $LOOP_PARTITION_LINUX $MNT
+sudo mkdir -p $MNT/boot/efi
+sudo mount $LOOP_PARTITION_EFI $MNT/boot/efi
 
 if [ -z "$COMMAND" ]; then
   echo "Press any key to continue"
@@ -27,6 +30,7 @@ else
   sleep 1
 fi
 
+sudo umount $MNT/boot/efi
 sudo umount $MNT
 sudo losetup -d $LOOP_DEV
 
