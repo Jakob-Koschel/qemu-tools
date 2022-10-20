0594dca736909866722d1433012af9e8a6be9f9b
Jakob Koschel <jkl820.git@gmail.com>
2022-10-20 11:55:11 +0200

    create separate create-chroot script

--- scripts/create-image.sh
+++ scripts/create-image.sh
@@ -1,160 +1,18 @@
 #!/usr/bin/env bash
-# Copyright 2016 syzkaller project authors. All rights reserved.
-# Use of this source code is governed by Apache 2 LICENSE that can be found in the LICENSE file.
 
-# create-image.sh creates a minimal Debian Linux image suitable for syzkaller.
+# create-image.sh creates a image based on a chroot environment.
 
 set -eux
 
-if [[ -z "$IMAGE" || -z "$DIR" ]]; then
-  echo "IMAGE and DIR need to be set!"
+IMAGE_SIZE="${IMAGE_SIZE:-2047}"
+
+if [ -z "$CHROOT" ]; then
+  echo "CHROOT need to be set!"
   exit 1
 fi
 
-# If ADD_PACKAGE is not defined as an external environment variable, use our default packages
-if [ -z ${ADD_PACKAGE+x} ]; then
-    ADD_PACKAGE="make,git,vim,tmux"
-fi
-
-# Variables affected by options
-ARCH=$(uname -m)
-RELEASE=bullseye
-FEATURE=minimal
-SEEK=2047
-
-# Display help function
-display_help() {
-    echo "Usage: $0 [option...] " >&2
-    echo
-    echo "   -a, --arch                 Set architecture"
-    echo "   -d, --distribution         Set on which debian distribution to create"
-    echo "   -f, --feature              Check what packages to install in the image, options are minimal, full"
-    echo "   -s, --seek                 Image size (MB), default 2048 (2G)"
-    echo "   -h, --help                 Display help message"
-    echo
-}
-
-while true; do
-    if [ $# -eq 0 ];then
-	echo $#
-	break
-    fi
-    case "$1" in
-        -h | --help)
-            display_help
-            exit 0
-            ;;
-        -a | --arch)
-	    ARCH=$2
-            shift 2
-            ;;
-        -d | --distribution)
-	    RELEASE=$2
-            shift 2
-            ;;
-        -f | --feature)
-	    FEATURE=$2
-            shift 2
-            ;;
-        -s | --seek)
-	    SEEK=$(($2 - 1))
-            shift 2
-            ;;
-        -*)
-            echo "Error: Unknown option: $1" >&2
-            exit 1
-            ;;
-        *)  # No more options
-            break
-            ;;
-    esac
-done
-
-# Handle cases where qemu and Debian use different arch names
-case "$ARCH" in
-    ppc64le)
-        DEBARCH=ppc64el
-        ;;
-    aarch64)
-        DEBARCH=arm64
-        ;;
-    arm)
-        DEBARCH=armel
-        ;;
-    x86_64)
-        DEBARCH=amd64
-        ;;
-    *)
-        DEBARCH=$ARCH
-        ;;
-esac
-
-# Create a minimal Debian distribution in a directory.
-# PREINSTALL_PKGS=linux-image-amd64,grub-pc
-PREINSTALL_PKGS=linux-image-$DEBARCH,grub-efi-$DEBARCH
-
-# Foreign architecture
-
-FOREIGN=false
-if [ $ARCH != $(uname -m) ]; then
-    # i386 on an x86_64 host is exempted, as we can run i386 binaries natively
-    if [ $ARCH != "i386" -o $(uname -m) != "x86_64" ]; then
-        FOREIGN=true
-    fi
-fi
-
-if [ $FOREIGN = "true" ]; then
-    # Check for according qemu static binary
-    if ! which qemu-$ARCH-static; then
-        echo "Please install qemu static binary for architecture $ARCH (package 'qemu-user-static' on Debian/Ubuntu/Fedora)"
-        exit 1
-    fi
-    # Check for according binfmt entry
-    if [ ! -r /proc/sys/fs/binfmt_misc/qemu-$ARCH ]; then
-        echo "binfmt entry /proc/sys/fs/binfmt_misc/qemu-$ARCH does not exist"
-        exit 1
-    fi
-fi
-
-# If full feature is chosen, install more packages
-if [ $FEATURE = "full" ]; then
-    PREINSTALL_PKGS=$PREINSTALL_PKGS","$ADD_PACKAGE
-fi
-
-sudo rm -rf $DIR
-sudo mkdir -p $DIR
-sudo chmod 0755 $DIR
-
-# 1. debootstrap stage
-
-DEBOOTSTRAP_PARAMS="--arch=$DEBARCH --include=$PREINSTALL_PKGS --components=main,contrib,non-free $RELEASE $DIR"
-if [ $FOREIGN = "true" ]; then
-    DEBOOTSTRAP_PARAMS="--foreign $DEBOOTSTRAP_PARAMS"
-fi
-
-sudo debootstrap $DEBOOTSTRAP_PARAMS
-
-# 2. debootstrap stage: only necessary if target != host architecture
-
-if [ $FOREIGN = "true" ]; then
-    sudo cp $(which qemu-$ARCH-static) $DIR/$(which qemu-$ARCH-static)
-    sudo chroot $DIR /bin/bash -c "/debootstrap/debootstrap --second-stage"
-fi
-
-# Set some defaults and enable promtless ssh to the machine for root.
-# sudo sed -i '/^root/ { s/:x:/::/ }' $DIR/etc/passwd
-# echo 'T0:23:respawn:/sbin/getty -L ttyS0 115200 vt100' | sudo tee -a $DIR/etc/inittab
-# printf '\nauto eth0\niface eth0 inet dhcp\n' | sudo tee -a $DIR/etc/network/interfaces
-# echo -en "127.0.0.1\tlocalhost\n" | sudo tee $DIR/etc/hosts
-# echo "nameserver 8.8.8.8" | sudo tee -a $DIR/etc/resolve.conf
-# echo "syzkaller" | sudo tee $DIR/etc/hostname
-
-# ssh-keygen -f $RELEASE.id_rsa -t rsa -N ''
-# sudo mkdir -p $DIR/root/.ssh/
-# cat $RELEASE.id_rsa.pub | sudo tee $DIR/root/.ssh/authorized_keys
-
 # Build a disk image
-dd if=/dev/zero of=$IMAGE bs=1M seek=$SEEK count=1
+dd if=/dev/zero of=$IMAGE bs=1M seek=$IMAGE_SIZE count=1
 
 # Set partition table to GPT (UEFI)
 parted -s $IMAGE -- mktable gpt
@@ -173,18 +31,9 @@ sudo partprobe $LOOP_DEV
 sudo mkfs.vfat -n EFI ${LOOP_PARTITION_EFI}
 sudo mkfs.ext4 -F $LOOP_PARTITION_LINUX
 
-# configure grub to make it bootable
-sudo sed -i 's/GRUB_CMDLINE_LINUX=\"\"/GRUB_CMDLINE_LINUX=\"console=tty0 console=ttyS0,115200n8\"/g' \
-    $DIR/etc/default/grub
-sudo sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT=\"quiet\"/GRUB_CMDLINE_LINUX_DEFAULT=\"\"/g' \
-    $DIR/etc/default/grub
-sudo sed -i 's/#GRUB_TERMINAL/GRUB_TERMINAL/g' $DIR/etc/default/grub
-
-sudo mkdir -p /mnt/$DIR
-sudo mount -o loop $LOOP_PARTITION_LINUX /mnt/$DIR
-sudo cp -a $DIR/. /mnt/$DIR/.
-sudo umount /mnt/$DIR
+sudo mkdir -p /mnt/$CHROOT
+sudo mount -o loop $LOOP_PARTITION_LINUX /mnt/$CHROOT
+sudo cp -a $CHROOT/. /mnt/$CHROOT/.
+sudo umount /mnt/$CHROOT
 
 sudo losetup -d $LOOP_DEV
-
-sudo rm -rf $DIR
--- scripts/enter-chroot.sh
+++ scripts/enter-chroot.sh
@@ -3,23 +3,23 @@ export HOME=/root
 export TMP=/tmp
 export TMPDIR=/tmp
 
-if [ -z "$DIR" ]; then
+if [ -z "$CHROOT" ]; then
   echo "DIR needs to be set!"
   exit 1
 fi
 
 for f in dev dev/pts proc sys ;
 do
-    if mountpoint -q $DIR/$f; then
-        echo "$DIR/$f already mounted, force unmount? [yN]"
+    if mountpoint -q $CHROOT/$f; then
+        echo "$CHROOT/$f already mounted, force unmount? [yN]"
         read answer
         if [ "$answer" != "${answer#[Yy]}" ];
         then
-            for f in sys proc dev/pts dev ; do sudo umount -lf $DIR/$f ; done
+            for f in sys proc dev/pts dev ; do sudo umount -lf $CHROOT/$f ; done
         fi
         exit 0
     else
-        sudo mount -o bind /$f $DIR/$f ;
+        sudo mount -o bind /$f $CHROOT/$f ;
     fi
 done
 
@@ -27,11 +27,11 @@ if [ -n "$1" ]; then
     # allow executing a range of commands
     cmds=$1
     set -- /bin/bash -l
-    echo "chroot $DIR \"$@\" -c \"$cmds\""
-    sudo chroot $DIR "$@" -c "$cmds"
+    echo "chroot $CHROOT \"$@\" -c \"$cmds\""
+    sudo chroot $CHROOT "$@" -c "$cmds"
 else
     set -- /bin/bash -l
-    sudo chroot $DIR "$@"
+    sudo chroot $CHROOT "$@"
 fi
 
-for f in sys proc dev/pts dev ; do sudo umount -lf $DIR/$f ; done
+for f in sys proc dev/pts dev ; do sudo umount -lf $CHROOT/$f ; done
