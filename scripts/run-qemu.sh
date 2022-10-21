2bc0794d5bcb6a8b90ba45db45b1962f6a1a3010
Jakob Koschel <jkl820.git@gmail.com>
2022-10-21 14:27:17 +0200

    run-qemu: make more arguments customizable & automatically set correct bios paths

--- scripts/run-qemu.sh
+++ scripts/run-qemu.sh
@@ -2,18 +2,46 @@
 
 # KERNEL="$HOME/Developer/linux/master"
 # KERNEL="${KERNEL}/arch/x86/boot/bzImage"
-# APPEND="console=ttyS0 nokaslr nosmp maxcpus=1 rcu_nocbs=0 nmi_watchdog=0 ignore_loglevel modules=sd-mod,usb-storage,ext4 rootfstype=ext4 earlyprintk=serial"
-MEMORY="8192"
+# KERNEL="vmlinuz"
+# INITRD="initrd.img"
+# APPEND="console=ttyS0 nokaslr nosmp maxcpus=1 rcu_nocbs=0 nmi_watchdog=0 ignore_loglevel modules=sd-mod,usb-storage,ext4 rootfstype=ext4 earlyprintk=serial net.ifnames=0"
+
+if [ $(uname -m) != $ARCH ]; then
+  # disable ACCEL if running different architecture
+  unset ENABLE_ACCEL
+fi
+
+if [ -n "$ENABLE_ACCEL" ]; then
+  # use the host cpu if accel is enabled
+  QEMU_CPU=${QEMU_CPU:-host}
+fi
+
+if [ "$(uname)" == "Darwin" ]; then
+  if [ -n "$ENABLE_ACCEL" ]; then ACCEL="hvf"; fi
+  if [ -n "$ENABLE_EFI" ]; then
+    QEMU_BIOS=${QEMU_BIOS:-"/opt/homebrew/share/qemu/edk2-$ARCH-code.fd"}
+  fi
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+  if [ -n "$ENABLE_ACCEL" ]; then ACCEL="kvm"; fi
+  if [ -n "$ENABLE_EFI" ]; then
+    if [ $ARCH = x86_64 ]; then
+      QEMU_BIOS=${QEMU_BIOS:-"/usr/share/ovmf/OVMF.fd"}
+    elif [ $ARCH = aarch64 ]; then
+      QEMU_BIOS=${QEMU_BIOS:-"/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"}
+    fi
+  fi
+else
+  echo "unsupported host"
+  exit 1
+fi
 
 if [ $ARCH = x86_64 ]; then
   QEMU_SYSTEM_x86_64="${QEMU_SYSTEM_x86_64:=qemu-system-x86_64}"
   QEMU_CPU="${QEMU_CPU:=qemu64,+smep,+smap}"
-  BIOS="$PWD/out/OVMF.fd"
 elif [ $ARCH = aarch64 ]; then
   QEMU_SYSTEM_x86_64="${QEMU_SYSTEM_x86_64:=qemu-system-aarch64}"
   QEMU_CPU="${QEMU_CPU:=max}"
   QEMU_MACHINE="${QEMU_MACHINE:=virt}"
-  BIOS="/usr/share/qemu-efi-aarch64/QEMU_EFI.fd"
 fi
 
 DRIVE="file=./${IMAGE},format=raw"
@@ -29,7 +57,8 @@ fi
 set -x
 
 $QEMU_SYSTEM_x86_64 \
-  ${BIOS:+ -bios ${BIOS}} \
+  ${ACCEL:+ -accel ${ACCEL}} \
+  ${QEMU_BIOS:+ -bios ${QEMU_BIOS}} \
   ${KERNEL:+ -kernel "${KERNEL}"} \
   ${INITRD:+ -initrd "${INITRD}"} \
   ${APPEND:+ -append "${APPEND}"} \
@@ -39,19 +68,18 @@ $QEMU_SYSTEM_x86_64 \
   ${HDA:+ -hda "${HDA}"} \
   ${ATTACH_GDB:+ -gdb tcp::${GDB_PORT}} \
   ${ATTACH_GDB:+ -S} \
-  ${ENABLE_KVM:+ -enable-kvm} \
   ${QEMU_MACHINE:+ -machine ${QEMU_MACHINE}} \
   -display none \
   -nographic \
-  -smp 1 \
+  -smp ${QEMU_CORES:-1} \
   ${QEMU_CPU:+ -cpu ${QEMU_CPU}} \
   ${SGX_EPC} \
   ${QEMU_9P_SHARED_FOLDER:+ -fsdev local,id=test_dev,path=${QEMU_9P_SHARED_FOLDER},security_model=none} \
   ${QEMU_9P_SHARED_FOLDER:+ -device virtio-9p-pci,fsdev=test_dev,mount_tag=test_mount} \
-  -m ${MEMORY} \
+  -m ${QEMU_MEMORY:-4096} \
   -echr 17 \
   -serial mon:stdio \
-  -snapshot \
+  ${ENABLE_SNAPSHOT:+ -snapshot} \
   -no-reboot
 
 # to mount 9p inside qemu:
